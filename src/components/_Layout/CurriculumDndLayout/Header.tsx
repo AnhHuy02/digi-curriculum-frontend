01c1c4ccc73cf240bd9b4adba2948a57211ad9f8
dtahuy02 <dtahuy02@gmail.com>
2022-07-08 02:25:03 +0700

    refactor(title): change title and animated background

--- src/components/_Layout/CurriculumDndLayout/Header.tsx
+++ src/components/_Layout/CurriculumDndLayout/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
       <Toolbar variant="dense">
         <Typography variant="h6" noWrap component="div" flexGrow={1}>
           <Link href="/" underline="none" color="white">
-            Interactive Syllabus
+            Digital Curriculum
           </Link>
         </Typography>
         <Button color="inherit">Login</Button>
--- src/components/_Layout/DashboardLayout/Header.tsx
+++ src/components/_Layout/DashboardLayout/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
       <Toolbar>
         <Typography variant="h6" noWrap component="div" flexGrow={1}>
           <Link href="/" underline="none" color="white">
-            Interactive Syllabus
+            Digital Curriculum
           </Link>
         </Typography>
         <Button color="inherit">Login</Button>
